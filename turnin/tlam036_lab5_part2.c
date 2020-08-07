a9f3505888299caf0af5e4c90c7dbdb2ac525525
ScorgeBSK <63446283+ScorgeBSK@users.noreply.github.com>
2020-08-07 00:38:02 -0700

    Update and rename tlam036_lab6_part2.c to tlam036_lab5_part2.c

--- turnin/tlam036_lab5_part2.c
+++ turnin/tlam036_lab5_part2.c
@@ -1,7 +1,7 @@
 /*	Author: Trung Lam
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #6  Exercise #2
+ *	Assignment: Lab #5  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
